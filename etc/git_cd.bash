933da5857e47f8123b6e5f0c0e1a08b118356222
sinsoku <sinsoku.listy@gmail.com>
2011-11-20 18:31:12 +0900

    ディレクトリのみ表示し、補完できるように修正

--- etc/git_cd.bash
+++ etc/git_cd.bash
@@ -12,19 +12,28 @@
 _git_cd()
 {
     if [ ${COMP_CWORD} -lt 2 ]; then
-        git_root="`git rev-parse --git-dir 2>/dev/null`/../"
+        git_repo="`git rev-parse --git-dir 2>/dev/null`"
         ret=$?
         if [ ${ret} -ne 0 ]; then return 0; fi
+        git_root="${git_repo%/[^/]*}/"
 
         cur="${COMP_WORDS[COMP_CWORD]}"
-        dist="${git_root}${cur}"
-        if [ -d "${dist}" ]; then
-            opts="`ls -a ${dist}`"
+        if [ -n "${cur##*/}" ]; then
+            cur_d="${cur}/"
         else
-            opts="`ls -a ${git_root}`"
+            cur_d="${cur}"
         fi
 
-        COMPREPLY=( ${opts} )
+        dist="${git_root}${cur_d}"
+        if [ -d "${dist}" ]; then
+            subs=`compgen -d "${dist}"`
+            opts=`for s in ${subs}; do echo ${s#${git_root}}; done`
+        else
+            subs=`compgen -d "${dist%/[^/]*}/"`
+            opts=`for s in ${subs}; do echo ${s#${git_root}}; done`
+        fi
+
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
     fi
 }
 complete -F _git_cd git_cd
