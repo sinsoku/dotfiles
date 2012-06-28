37825ecd22d8d0ffdc535d30b6d427a14b05b696
sinsoku <sinsoku.listy@gmail.com>
2012-06-28 19:50:33 +0900

    ruby関連のファイルを追加

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@
 
 shell_path=$(cd $(dirname $0);pwd)
 
+# git
 ln -s ${shell_path}/git/.gitconfig ~/
 ln -s ${shell_path}/git/.gitignore_global ~/
+
+# ruby
+ln -s ${shell_path}/ruby/.railsrc ~/
+ln -s ${shell_path}/ruby/rails_template ~/
