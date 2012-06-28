3eda6228a414c05e556d33631029d816eda6f4b2
sinsoku <sinsoku.listy@gmail.com>
2012-06-29 00:13:31 +0900

    railsのtemplate関連のファイルを追加

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 shell_path=$(cd $(dirname $0);pwd)
 
 # git
-ln -s ${shell_path}/git/.gitconfig ~/
-ln -s ${shell_path}/git/.gitignore_global ~/
+ln -fs ${shell_path}/git/.gitconfig ~/
+ln -fs ${shell_path}/git/.gitignore_global ~/
+ln -fs ${shell_path}/git/gitignore ~/
 
 # ruby
-ln -s ${shell_path}/ruby/.railsrc ~/
-ln -s ${shell_path}/ruby/rails_template ~/
+ln -fs ${shell_path}/ruby/.gemrc ~/
+ln -fs ${shell_path}/ruby/.railsrc ~/
+ln -fs ${shell_path}/ruby/rails_template ~/
+ln -fs ${shell_path}/git/gitignore ${shell_path}/ruby/rails_template/
